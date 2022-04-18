c937e421c9bff1f9cb00bac03a9650770e9c0630
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-04-19 00:30:40 +0300

    fixing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
+/*import { UsersComponent } from './users/users.component';*/
 import {  UsersService } from "./users.service";
-import { NgProgressModule } from '@ngx-progressbar/core';
-import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
+/*import { NgProgressModule } from '@ngx-progressbar/core';*/
+/*import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';*/
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
     NavbarComponent,
-    ])},
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    NgProgressModule.forRoot(),
-    NgProgressHttpClientModule
-  ],
+],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/repos.ts
+++ src/app/repos.ts
@@ -2,4 +2,4 @@ export class Repos {
 
     constructor(public name:string, public description:string, public html_url: string, public created_at:Date){
 
-    }
+    }}
