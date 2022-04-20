a4f57853a077b0976c6595bc964a4f81497ff3ad
Moglie02 <salma.ntidendereza@student.moringaschool.com>
2022-04-20 23:10:48 +0300

    USERCOMPONENT CHANGES

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProfilesComponent } from './profiles/profiles.component';
 import { RepositoryComponent } from './repository/repository.component';
-
+import { UserComponent } from './user/user.component';
 const routes: Routes = [
   {path: '', redirectTo: 'profiles', pathMatch: 'full'},
   {path: 'profiles', component: ProfilesComponent  },
   {path: 'repository', component: RepositoryComponent},
+  {path: 'users', component: UserComponent},
 ];
 
 @NgModule({
